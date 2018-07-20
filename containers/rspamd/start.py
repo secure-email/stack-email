cd0240ef76b85a513a75c22ef89d07ac6f9a5b68
Joel Gilley <gilleyj@gmail.com>
2018-07-20 11:57:06 -0400

    Rspamd needs fixing.... starts for now

--- containers/rspamd/start.py
+++ containers/rspamd/start.py
@@ -8,7 +8,7 @@ import glob
 convert = lambda src, dst: open(dst, "w").write(jinja2.Template(open(src).read()).render(**os.environ))
 
 # Actual startup script
-os.environ["FRONT_ADDRESS"] = socket.gethostbyname("front")
+os.environ["FRONT_ADDRESS"] = socket.gethostbyname("rspamd")
 
 for rspamd_file in glob.glob("/conf/*"):
     convert(rspamd_file, os.path.join("/etc/rspamd/local.d", os.path.basename(rspamd_file)))
