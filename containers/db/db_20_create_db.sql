05f451523ad46097d574c507e6e34f49d1ab6cbf
Joel Gilley <gilleyj@gmail.com>
2018-07-23 16:40:43 -0400

    ipv6 note

--- containers/db/db_20_create_db.sql
+++ containers/db/db_20_create_db.sql
@@ -7,3 +7,14 @@ CREATE TABLE `domain` (
   `name` varchar(255) NOT NULL,
   UNIQUE(name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*
+mysql> SELECT HEX(INET6_ATON('fdfe::5a55:caff:fefa:9089'));
+        -> 'FDFE0000000000005A55CAFFFEFA9089'
+mysql> SELECT HEX(INET6_ATON('10.0.5.9'));
+        -> '0A000509'
+  function has the VARBINARY data type: VARBINARY(16) for IPv6 addresses and
+  VARBINARY(4) for IPv4 addresses. If the argument is not a valid address, INET6_ATON() returns NULL.
+
+
+  */
